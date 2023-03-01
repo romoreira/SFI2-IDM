648414af99434820e47603ab92a26b93b1f5289a
ubuntu <rodrigl.moreira206@gmail.com>
2023-03-01 23:58:25 +0000

    "API almost done"

--- mirror/docker-image/prediction.py
+++ mirror/docker-image/prediction.py
@@ -20,16 +20,49 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+import pickle
+
+
+def load_model(model_name):
+    # load the model from disk
+    knn = pickle.load(open(model_name, 'rb'))
+    return knn
+
+def clean_dataset(df):
+    df = df.drop(['src_ip', 'dst_ip', 'timestamp', 'flow_byts_s'], axis=1)
+
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(axis=1)
+    return df[indices_to_keep].astype(np.float64)
+
+def do_predct(json_string):
+    print("JSON RECEBIDO NO DO_PREDICT:" +str(json_string))
+    #dict = json.loads(json_string)
+    dict = json_string
+    df = pd.DataFrame([], columns=dict['columns'])
+    list = dict['data'][0]
+    df = df.append(pd.DataFrame([list], columns=dict['columns']), ignore_index=True)
+    X_test = clean_dataset(df)
+    knn = load_model("saved_model/knn.pth")
+    y_pred = knn.predict(X_test)
+    print(y_pred)
+    return y_pred
+
+
 
 @app.route('/prediction', methods=['POST'])
 def predition_task():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        json_string = request.json
+        json_string = request.get_json()
         print("JSON Recebido pelo prediction: "+str(json_string))
         return {'result': 'ok'}
     elif request.data:
         print('Data:' + str(request.data))
+        json_string = request.get_json()
+        prediction_response = do_predct(json_string)
+
         json_string = '{"src_ip": "SOURCE_IP", "src_port": "SOURCE_PORT", "dst_ip": "DST_IP", "dst_port": "DST_PORT", "result": [0], "probability": [[0]]}'
         json_string = json.loads(json_string)
         print("Posting json_string to: "+str(json_string))
